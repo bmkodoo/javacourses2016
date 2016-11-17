fb1be5b6f2b44988f74d69474fc8dec23165291b
eseev-german <eseev.german@yandex.ru>
2016-11-18 00:24:18 +0300

    90% of Task_2, without test

--- src/main/java/com/epam/javacourses2016/task2/SolverTask2.java
+++ src/main/java/com/epam/javacourses2016/task2/SolverTask2.java
@@ -2,6 +2,7 @@ package com.epam.javacourses2016.task2;
 
 import java.io.File;
 import java.util.Set;
+import java.util.TreeSet;
 
 /**
  * Сформировать множество элементов, входящих в каталог и его подкаталоги.
@@ -10,11 +11,20 @@ public class SolverTask2 {
 
     /**
      * Формирует множество всех элементов, входящих в текущий каталог и во все вложенные.
+     *
      * @param directory Корневой каталог.
      * @return Множество элементов корневого каталога и подкаталогов.
      */
     public Set<File> getFiles(File directory) {
-        //TODO: realize solver of a task
-        return null;
+        Set<File> fileset = new TreeSet();
+        File[] folderfiles = directory.listFiles();
+        for (File file : folderfiles) {
+            if (file.isDirectory()) {
+                fileset.addAll(getFiles(file));
+                continue;
+            }
+            fileset.add(file);
+        }
+        return fileset;
     }
 }
--- src/test/java/com/epam/javacourses2016/task2/Task2Test.java
+++ src/test/java/com/epam/javacourses2016/task2/Task2Test.java
@@ -1,11 +1,13 @@
 package com.epam.javacourses2016.task2;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Task2Test {
 
     @Test
     public void testTask2() {
-        //TODO: make some smart test
+
+
     }
 }
