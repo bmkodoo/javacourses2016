4791d0cbe00777b546a7f705826fcb721698d183
eseev-german <eseev.german@yandex.ru>
2016-11-23 17:20:53 +0300

    Task3 done

--- src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
+++ src/main/java/com/epam/javacourses2016/task3/SolverTask3.java
@@ -1,5 +1,6 @@
 package com.epam.javacourses2016.task3;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Set;
 
@@ -16,7 +17,15 @@ public class SolverTask3 {
      * @return Список, упорядоченных по длине строк, составляющих стихотворения автора.
      */
     public List<String> sortPoems(Set<Poem> poems, String author) {
-        //TODO: realize solver of a task
-        return null;
+        List<String> sortedpoems=new ArrayList<>();
+        for (Poem poem: poems
+             ) {
+            if(author.equals(poem.getAuthor())){
+                sortedpoems.addAll(poem.getLines());
+            }
+        }
+
+        sortedpoems.sort((String a, String b) -> (Integer.valueOf(a.length()).compareTo(b.length())));
+        return sortedpoems;
     }
 }
