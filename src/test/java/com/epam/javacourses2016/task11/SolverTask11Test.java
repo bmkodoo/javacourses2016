5a9081e60766543d08803eb0e75a2a3d081f92be
eseev-german <eseev.german@yandex.ru>
2016-12-04 19:55:06 +0300

    if i have understood you correctly

--- src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
+++ src/test/java/com/epam/javacourses2016/task11/SolverTask11Test.java
@@ -1,42 +1,43 @@
 package com.epam.javacourses2016.task11;
 
-import junit.framework.Assert;
+import com.epam.javacourses2016.task8.SolverTask8;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
+import java.util.List;
 
 import static org.testng.Assert.*;
 
 public class SolverTask11Test {
-
-    @Test(enabled = false, dataProvider = "listofpeople")
-    public void testEmulate(String[] members, String winner) throws Exception {
-        SolverTask11 solver=new SolverTask11();
-        Assert.assertEquals(solver.emulate(new ArrayList<String>(Arrays.asList(members))),winner);
+    @Test(enabled = true, dataProvider = "people")
+    public void testEmulate(String[] arr, String result) throws Exception {
+        SolverTask11 task11 = new SolverTask11();
+        Assert.assertEquals(task11.emulate(convertToLinkedList(arr)), result);
     }
 
-    @Test(enabled = false, dataProvider = "listofpeople")
-    public void testEmulate1(String[] members, String winner) throws Exception {
-        SolverTask11 solver=new SolverTask11();
-        Assert.assertEquals(solver.emulate(new LinkedList<String>(Arrays.asList(members))),winner);
-
+    @Test(enabled = false, dataProvider = "people")
+    public void testEmulate1(String[] arr, String result) throws Exception {
+        SolverTask11 task11 = new SolverTask11();
+        Assert.assertEquals(task11.emulate(convertToLinkedList(arr)), result);
     }
 
-    /**
-     * Удаление происходит, начиная с первого (условие задачи)
-     *
-     */
+    public ArrayList<String> convertToArrayList(String[] arr) {
+        return new ArrayList(Arrays.asList(arr));
+    }
 
-@DataProvider(name ="listofpeople")
-    private Object[][] peoples(){
-    return new Object[][]{
-            {new String[]{"a", "b", "c", "d", "e","f"}, "d"},
-            {new String[]{"a"}, "a"},
-            {new String[]{"Bob", "Rob", "Dan", "Stan", "Ron","Victor", "Don"}, "Victor"},
-    };
-}
+    public LinkedList<String> convertToLinkedList(String[] arr) {
+        return new LinkedList(Arrays.asList(arr));
+    }
+
+    @DataProvider(name = "people")
+    private Object[][] peoples() {
+        return new Object[][]{
+                {new String[]{"Vanya", "Nikita", "Pasha", "Alex", "Maxim", "Anton", "Petya", "Dima"}, "Dima"},
+                {new String[]{"Anton", "Petya", "Dima", "Vanya", "Nikita"}, "Petya"}
+        };
+    }
 }
